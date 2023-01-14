52369499a9e99cc0add7e12090589ca8cb213b91
LonelyDry <monteggiamati94@gmail.com>
2023-01-13 21:04:23 -0300

    Proporcionamos el servicio a los componentes education y projects

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../service/data.service';
+
 
 @Component({
   selector: 'app-education',
-  templateUrl: './education.component.html',
+  templateUrl: '../././projects/projects.component.html',
   styleUrls: ['./education.component.css']
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit {
+
+  data: any;
+
+
+  constructor(
+    private DataService: DataService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.DataService.getTasks().subscribe((Portfolio) => (
+      this.data = Portfolio.education[0]
+    ));
+
+  }
+
 
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../service/data.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+
+  data: any;
+
+
+  constructor(
+    private DataService: DataService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.DataService.getTasks().subscribe((Portfolio) => (
+      this.data = Portfolio.projects[0]
+    ));
+
+  }
+
 
 }
